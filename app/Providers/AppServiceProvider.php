4157a50c4efc959d50b5ef1d49ab6216c338d6ab
mauriciomaxuser <mariosaquinga026@gmail.com>
2026-01-01 16:43:02 -0500

    cambios reales mas reales aun coregido

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,10 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Forzar el uso de HTTPS en producción
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
         //
     }
 }
