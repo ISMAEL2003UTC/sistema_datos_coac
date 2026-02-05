d4a38b3ab8665773fd48fe6520c8fda8476bbb20
mauriciomaxuser <mariosaquinga026@gmail.com>
2026-02-05 14:28:05 -0500

    cambios con verificacion mc

--- app/Mail/VerificarCorreoUsuario.php
+++ app/Mail/VerificarCorreoUsuario.php
@@ -3,32 +3,31 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldQueue; // <- esto
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Usuario;
 
-class VerificarCorreoUsuario extends Mailable implements ShouldQueue
+class VerificarCorreoUsuario extends Mailable implements ShouldQueue // <- aquí
 {
     use Queueable, SerializesModels;
 
     public $usuario;
-    public $url;
 
     public function __construct(Usuario $usuario)
     {
         $this->usuario = $usuario;
-
-        // URL segura y correcta (funciona en Railway, local y prod)
-        $this->url = route(
-            'verificar.correo',
-            $this->usuario->email_verificacion_token
-        );
     }
 
     public function build()
     {
+    $url = url('/verificar-correo/' . $this->usuario->email_verificacion_token);
+
         return $this->subject('Verificación de correo electrónico')
-                    ->view('emails.verificar_correo');
+                    ->view('emails.verificar_correo')
+                    ->with([
+                        'usuario' => $this->usuario,
+                        'url' => $url
+                    ]);
     }
 }
