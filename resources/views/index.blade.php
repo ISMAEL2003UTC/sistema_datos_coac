33b18672192f82dad0a49fe3a26892981751e7c6
mauriciomaxuser <mariosaquinga026@gmail.com>
2026-02-05 22:51:09 -0500

    cambios del boton logout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gestión de Datos Personales - COAC</title>
     
-    <form action="{{ route('logout') }}" method="POST" style="position:absolute; top:20px; right:20px;">
-        @csrf
-        <button type="submit" class="btn btn-danger">
-            <i class="fa-solid fa-right-from-bracket"></i> Cerrar sesión
-        </button>
-    </form>
+    
     
 
     <link rel="stylesheet" href="{{ asset('styles/styles.css') }}">
@@ -46,6 +41,14 @@
 
 </style>
 <body>
+    <!-- Botón de logout -->
+    <form action="{{ route('logout') }}" method="POST" class="logout-button">
+        @csrf
+        <button type="submit" class="btn btn-danger">
+            <i class="fa-solid fa-right-from-bracket"></i> Cerrar sesión
+        </button>
+    </form>
+    
     <div class="container">
         <div class="header">
             <h1> Sistema de Gestión de Datos Personales</h1>
